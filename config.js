073268930697c87bf51dd26d21b6172d5943b833
LORD PREMO <cypherx599@gmail.com>
2026-02-28 16:27:34 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+export default {
+    prefix: ['.', '!', '/'], 
+    defaultPrefix: '.', 
+
+    projectOwner: {
+        number: "255773001107",
+        name: "BROKENLORD"
+    },
+
+    bot: {
+        name: "BROKEN LORD MD",
+        mode: "private", 
+        version: "1.0.0"
+    }
+};
